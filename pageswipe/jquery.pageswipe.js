c6488590b89ced81494b78d9b30765e41f372885
Michiel Sikkes <michiel@firmhouse.com>
2012-05-22 21:40:21 +0200

    Set up some prototype values for the plugin to center the viewport

--- pageswipe/jquery.pageswipe.js
+++ pageswipe/jquery.pageswipe.js
@@ -1,6 +1,14 @@
 (function( $ ) {
   $.fn.pageSwipe = function() {
-    this.wrap('<div class="pageswipe-viewport"></div>')
+    
+    viewport = $('<div class="pageswipe-viewport" class="viewport"></div>')
+    viewport.css({height: '1024px', width: '768px', margin: 'auto', overflow: "hidden", position: "relative"})
+    this.wrap(viewport)
+    
+    numberOfPages = this.children('.page').length
+    
+    this.css({width: (numberOfPages * 768) + "px", position: "absolute", height: "1024px", overflow: 'hidden'})
+    this.children('.page').css({width: '768px', height: '1024px', "float": "left"})
     
     return this
   };
