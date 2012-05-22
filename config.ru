b2b0b4cc5c7ac63b2a6857a7356102d668b8ba56
Michiel Sikkes <michiel@firmhouse.com>
2012-05-22 22:45:47 +0200

    Initial version for scrolling horizontally

--- config.ru
+++ config.ru
@@ -9,5 +9,6 @@ run lambda { |env|
       'Content-Type'  => 'text/html', 
       'Cache-Control' => 'public, max-age=86400' 
     },
+    File.open('demo/index.html', File::RDONLY)
   ]
 }
--- pageswipe/jquery.pageswipe.js
+++ pageswipe/jquery.pageswipe.js
@@ -10,6 +10,24 @@
     this.css({width: (numberOfPages * 768) + "px", position: "absolute", height: "1024px", overflow: 'hidden'})
     this.children('.page').css({width: '768px', height: '1024px', "float": "left"})
     
+    var that = this
+    var pagesStartPosition = 0
+    var horizontalTouchStart = null
+    
+    $(document).bind('touchstart.pageswipe', function(event) {
+      e = event.originalEvent
+      
+      pagesStartPosition = that.position().left
+      horizontalTouchStart = e.touches[0].pageX
+    })
+    
+    $(document).bind('touchmove.pageswipe', function(event) {
+      horizontalTouchPosition = e.touches[0].pageX
+      horizontalTouchDelta = horizontalTouchPosition - horizontalTouchStart
+      
+      that.css({"-webkit-transform": "translate3d(" + (pagesStartPosition + horizontalTouchDelta) + "px,0,0)"})
+    })
+    
     return this
   };
 })( jQuery )
