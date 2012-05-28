b851cdef5439ded338d1dad86bd75c15c6611de6
Michiel Sikkes <michiel@firmhouse.com>
2012-05-28 15:28:26 +0200

    Recognize horizontal scroll direction and implemented animating to next and previous pages

--- pageswipe/jquery.pageswipe.js
+++ pageswipe/jquery.pageswipe.js
@@ -13,19 +13,63 @@
     var that = this
     var pagesStartPosition = 0
     var horizontalTouchStart = null
+    var scrollDirection = null
+    var currentPage = 1
+    
+    $(this).bind('webkitTransitionEnd', function () {
+      $(this).css({"-webkit-transition": ""})
+    })
     
     $(document).bind('touchstart.pageswipe', function(event) {
       e = event.originalEvent
+      
+      scrollDirection = null
       
       pagesStartPosition = that.position().left
       horizontalTouchStart = e.touches[0].pageX
     })
     
     $(document).bind('touchmove.pageswipe', function(event) {
+      e = event.originalEvent
+      
       horizontalTouchPosition = e.touches[0].pageX
       horizontalTouchDelta = horizontalTouchPosition - horizontalTouchStart
       
-      that.css({"-webkit-transform": "translate3d(" + (pagesStartPosition + horizontalTouchDelta) + "px,0,0)"})
+      if (!scrollDirection) {
+        /* We do not have a scrollDirection yet. So we are going to figure it
+           out: */
+        if (Math.abs(horizontalTouchDelta) > 10) {
+          scrollDirection = "h"
+        }
+      }
+      
+      if (scrollDirection == "h") {
+        e.preventDefault()
+        that.css({"-webkit-transform": "translate3d(" + (pagesStartPosition + horizontalTouchDelta) + "px,0,0)"})
+      }
+    })
+    
+    $(document).bind('touchend.pageswipe', function(event) {
+      e = event.originalEvent
+      
+      horizontalTouchEnd = e.changedTouches[0].pageX
+      horizontalTouchDelta = horizontalTouchStart - horizontalTouchEnd
+      
+      if (scrollDirection == "h") {
+        if (horizontalTouchDelta > 0) {
+          that.css({
+            "-webkit-transition": "all .2s ease-in-out", 
+            "-webkit-transform": "translate3d("  + (-768 * (currentPage)) + "px,0,0)"
+          })
+          currentPage++
+        } else if (horizontalTouchDelta < 0) {
+          currentPage--
+          that.css({
+            "-webkit-transition": "all .2s ease-in-out", 
+            "-webkit-transform": "translate3d("  + (-768 * (currentPage - 1)) + "px,0,0)"
+          })
+        }
+      }
     })
     
     return this
