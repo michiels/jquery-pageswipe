a1c470a0064f0c3ff58aa826aa69f621e3ce3d25
Michiel Sikkes <michiel@firmhouse.com>
2012-06-03 18:44:55 +0200

    Revert

--- pageswipe/jquery.pageswipe.js
+++ pageswipe/jquery.pageswipe.js
@@ -1,110 +1,105 @@
 (function( $ ) {
-  $.fn.pageSwipe = function( options ) {
+  $.fn.pageSwipe = function( ) {
     
-    var settings = $.extend( {
-      'autoScale' : false
-    }, options)
+    viewportHeight = $(window).height();
     
-    return this.each(function() {
+    viewport = $('<div class="pageswipe-viewport" class="viewport"></div>')
+    viewport.css({height: viewportHeight + 'px', width: '768px', margin: 'auto', overflow: "hidden", position: "relative"})
+    this.wrap(viewport)
     
-      viewportHeight = $(window).height();
+    var numberOfPages = this.children('.page').length
     
-      viewport = $('<div class="pageswipe-viewport" class="viewport"></div>')
-      viewport.css({height: viewportHeight + 'px', width: '768px', margin: 'auto', overflow: "hidden", position: "relative"})
-      this.wrap(viewport)
+    this.css({width: (numberOfPages * 768) + "px", position: "absolute", height: viewportHeight + 'px', overflow: 'hidden'})
+    this.children('.page').css({width: '768px', height: viewportHeight + 'px', "float": "left", "overflow-y": "auto", "-webkit-overflow-scrolling": "touch"})
     
-      var numberOfPages = this.children('.page').length
+    var that = this
+    var pagesStartPosition = 0
+    var horizontalTouchStart = null
+    var verticalTouchStart = null
+    var scrollDirection = null
+    var currentPage = 1
     
-      this.css({width: (numberOfPages * 768) + "px", position: "absolute", height: viewportHeight + 'px', overflow: 'hidden'})
-      this.children('.page').css({width: '768px', height: viewportHeight + 'px', "float": "left", "overflow-y": "auto", "-webkit-overflow-scrolling": "touch"})
+    $(this).bind('webkitTransitionEnd', function () {
+      $(this).css({"-webkit-transition": ""})
+    })
     
-      var that = this
-      var pagesStartPosition = 0
-      var horizontalTouchStart = null
-      var verticalTouchStart = null
-      var scrollDirection = null
-      var currentPage = 1
-    
-      $(this).bind('webkitTransitionEnd', function () {
-        $(this).css({"-webkit-transition": ""})
-      })
-    
-      $(document).bind('touchstart.pageswipe', function(event) {
-        e = event.originalEvent
+    $(document).bind('touchstart.pageswipe', function(event) {
+      e = event.originalEvent
       
-        scrollDirection = null
+      scrollDirection = null
       
-        pagesStartPosition = that.position().left
-        horizontalTouchStart = e.touches[0].pageX
-        verticalTouchStart = e.touches[0].pageY
-      })
+      pagesStartPosition = that.position().left
+      horizontalTouchStart = e.touches[0].pageX
+      verticalTouchStart = e.touches[0].pageY
+    })
     
-      $(document).bind('touchmove.pageswipe', function(event) {
-        e = event.originalEvent
+    $(document).bind('touchmove.pageswipe', function(event) {
+      e = event.originalEvent
       
-        horizontalTouchPosition = e.touches[0].pageX
-        horizontalTouchDelta = horizontalTouchPosition - horizontalTouchStart
+      horizontalTouchPosition = e.touches[0].pageX
+      horizontalTouchDelta = horizontalTouchPosition - horizontalTouchStart
       
-        verticalTouchPosition = e.touches[0].pageY
-        verticalTouchDelta = verticalTouchPosition - verticalTouchStart
+      verticalTouchPosition = e.touches[0].pageY
+      verticalTouchDelta = verticalTouchPosition - verticalTouchStart
       
-        if (horizontalTouchDelta > 0) {
-          horizontalDirection = "right"
-        } else if (horizontalTouchDelta < 1) {
-          horizontalDirection = "left"
+      if (horizontalTouchDelta > 0) {
+        horizontalDirection = "right"
+      } else if (horizontalTouchDelta < 1) {
+        horizontalDirection = "left"
+      }
+      
+      if (!scrollDirection) {
+        /* We do not have a scrollDirection yet. So we are going to figure it
+           out: */
+        if (Math.abs(horizontalTouchDelta) > 10) {
+          scrollDirection = "h"
         }
-      
-        if (!scrollDirection) {
-          /* We do not have a scrollDirection yet. So we are going to figure it
-             out: */
-          if (Math.abs(horizontalTouchDelta) > 10) {
-            scrollDirection = "h"
-          }
         
-          if (Math.abs(verticalTouchDelta) > 10) {
-            scrollDirection = "v"
-          }
+        if (Math.abs(verticalTouchDelta) > 10) {
+          scrollDirection = "v"
         }
+      }
       
-        if (scrollDirection == "h") {
-          e.preventDefault() /* Disable other scroll directions when we are scrolling horizontally */
-          if ( 
-            (horizontalDirection == "right" && currentPage > 1) || /* Make sure we cannot scroll to the left of the first page. */
-            (horizontalDirection == "left" && currentPage < numberOfPages) /* Make sure we cannot scroll beyond the last page. */
-          ) {
-            that.css({"-webkit-transform": "translate3d(" + (pagesStartPosition + horizontalTouchDelta) + "px,0,0)"})
-          }
-        } else if (scrollDirection == "v") {
+      if (scrollDirection == "h") {
+        e.preventDefault() /* Disable other scroll directions when we are scrolling horizontally */
+        if ( 
+          (horizontalDirection == "right" && currentPage > 1) || /* Make sure we cannot scroll to the left of the first page. */
+          (horizontalDirection == "left" && currentPage < numberOfPages) /* Make sure we cannot scroll beyond the last page. */
+        ) {
+          that.css({"-webkit-transform": "translate3d(" + (pagesStartPosition + horizontalTouchDelta) + "px,0,0)"})
         }
-      })
+      } else if (scrollDirection == "v") {
+      }
+    })
     
-      $(document).bind('touchend.pageswipe', function(event) {
-        e = event.originalEvent
+    $(document).bind('touchend.pageswipe', function(event) {
+      e = event.originalEvent
       
-        horizontalTouchEnd = e.changedTouches[0].pageX
-        horizontalTouchDelta = horizontalTouchStart - horizontalTouchEnd
+      horizontalTouchEnd = e.changedTouches[0].pageX
+      horizontalTouchDelta = horizontalTouchStart - horizontalTouchEnd
       
-        if (scrollDirection == "h") {
-          if (horizontalTouchDelta > 0 && currentPage < numberOfPages) {
-            that.css({
-              "-webkit-transition": "all .2s ease-in-out", 
-              "-webkit-transform": "translate3d("  + (-768 * (currentPage)) + "px,0,0)"
-            })
-            currentPage++
-          } else if (horizontalTouchDelta < 0 && currentPage > 1) {
-            currentPage--
-            that.css({
-              "-webkit-transition": "all .2s ease-in-out", 
-              "-webkit-transform": "translate3d("  + (-768 * (currentPage - 1)) + "px,0,0)"
-            })
-          }
+      if (scrollDirection == "h") {
+        if (horizontalTouchDelta > 0 && currentPage < numberOfPages) {
+          that.css({
+            "-webkit-transition": "all .2s ease-in-out", 
+            "-webkit-transform": "translate3d("  + (-768 * (currentPage)) + "px,0,0)"
+          })
+          currentPage++
+        } else if (horizontalTouchDelta < 0 && currentPage > 1) {
+          currentPage--
+          that.css({
+            "-webkit-transition": "all .2s ease-in-out", 
+            "-webkit-transform": "translate3d("  + (-768 * (currentPage - 1)) + "px,0,0)"
+          })
         }
+      }
       
-        $('.page').css({
-            "overflow-y": "hidden",
-            "-webkit-overflow-scrolling": "none"
-        })
+      $('.page').css({
+          "overflow-y": "hidden",
+          "-webkit-overflow-scrolling": "none"
       })
-    }
+    })
+    
+    return this
   };
 })( jQuery )
